5ad6e287721fecaf926df1da6ca6b735603ec99d
adeel sheikh <adeel.zafar.ahmed@gmail.com>
2023-11-08 14:44:55 +0500

    chnagesad

--- lp-publishing-4/index.php
+++ lp-publishing-4/index.php
@@ -9,7 +9,8 @@
     <title>Amazon Publishing Official: #1 Book Writing, Editing & Publishing Services</title>
     <link rel="stylesheet" type="text/css" href="assets/css/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css">
     <link rel="icon" type="image/x-icon" href="favicon.png">
 </head>
 
@@ -1148,27 +1149,7 @@
     });
 }
     </script>
-<script>
-var isloading = true;
 
-function removeLoading() {
-    if (isloading) {
-        $("head").append(`
-                  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css">
-`)
-
-        $('.myLazy').each(function() {
-            $(this).attr("src", $(this).attr('data-src'))
-        })
-        isloading = false
-    }
-}
-$(document).mousemove(removeLoading)
-$(document).scroll(removeLoading)
-$(document).on("touchmove", removeLoading)
-setInterval(removeLoading, 10000);
-</script>
 
 </body>
 
