07f4e130a38fc5c061e5e007a477911dd1953d51
abrarraza1 <abrar123raza@gmail.com>
2024-02-16 02:24:55 +0500

    first commit

--- publish-your-book-now-2/assets/js/socketPackage/online-offline.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('visibilitychange',function(){
-    if ( document.visibilityState === 'hidden' ) {
-       socketHelper.tabSwitch("OFFLINE");
-    } else {
-      socketHelper.tabSwitch("ONLINE");
-    }
- });
